a2c3472333ca947725a762a730c56bb7f14c72f4
Simanga <simacoder@hotmail.com>
2025-03-27 22:30:24 +0200

    maximum pair production solution

--- sol_two_sum.py
+++ sol_two_sum.py
@@ -1,8 +1,8 @@
 # algorithmic tool course
-num1 = int(input())
-num2 = int(input())
+
+num1, num2 = map(int, input().split())
 def two_sum(num1, num2):
-    sum = num1 + num2
-    return sum
+    return  num1 + num2
+
 
 print(two_sum(num1, num2))
